280519ccc8328e50c63e89d89f93a3872083a809
LucasLCabral <lucasludovicocabraal@gmail.com>
2024-06-13 09:46:09 -0300

    iniciando repositorio

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// popup.js
+
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
