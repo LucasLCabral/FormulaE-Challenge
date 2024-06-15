5e90449503fd788d2765ce11cb24e5965e168036
weslley <weslleycardoso@gmail.com>
2024-06-15 18:41:06 -0300

    teste

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,16 @@ let x = document.getElementById("musica")
 function som(){
     x.play();
 }
+
+window.addEventListener('scroll', function() {
+    const elements = document.querySelectorAll('.section-2, .section-titulo');
+    const screenPosition = window.innerHeight / 1.2;
+
+    elements.forEach(element => {
+        const elementPosition = element.getBoundingClientRect().top;
+
+        if (elementPosition < screenPosition) {
+            element.classList.add('appear');
+        }
+    });
+});
